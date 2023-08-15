e902faf5897f143acaaa5ba23348de9320e51339
Paweł Durkalec <pdurkalec1006@gmail.com>
2023-08-15 20:17:01 +0200

    add menu, add listing

--- SSH-Configurer-UI/Services/DeviceService.cs
+++ SSH-Configurer-UI/Services/DeviceService.cs
@@ -1,6 +1,22 @@
-﻿namespace SSH_Configurer_UI.Services
+﻿using SSH_Configurer_UI.Data;
+using static SSH_Configurer_UI.Data.DataTypes;
+
+namespace SSH_Configurer_UI.Services
 {
     public class DeviceService
     {
+        private static readonly Device[] Devices = new[]
+        {
+            new Device(0,"Computer1","127.0.56.2", 22, 0, "adminadmin"),
+            new Device(1,"Computer2","127.0.56.3", 22, 0, "adminadmin"),
+            new Device(2,"Computer3","127.0.56.4", 22, 0, "adminadmin"),
+            new Device(3,"Computer4","127.0.56.5", 22, 1, "adminadmin"),
+            new Device(4,"Computer5","127.0.56.6", 22, 1, "adminadmin")
+        };
+
+        public List<Device> GetAllDevices()
+        {
+            return new List<Device>(Devices);
+        }
     }
 }
--- SSH-Configurer-UI/Services/GroupService.cs
+++ SSH-Configurer-UI/Services/GroupService.cs
@@ -1,6 +1,19 @@
-﻿namespace SSH_Configurer_UI.Services
+﻿using static SSH_Configurer_UI.Data.DataTypes;
+
+namespace SSH_Configurer_UI.Services
 {
     public class GroupService
     {
+        private static readonly Group[] Groups = new[]
+        {
+            new Group(0, "Room1", 0, new List<int>(){0,1,2}),
+            new Group(1, "Room1", 1, new List<int>(){3,4})
+
+        };
+
+        public List<Group> GetAllGroups()
+        {
+            return new List<Group>(Groups);
+        }
     }
 }
