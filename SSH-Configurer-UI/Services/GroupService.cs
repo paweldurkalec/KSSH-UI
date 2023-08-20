bb16533c11195ee0081b8df4bb22735579406068
Paweł Durkalec <pdurkalec1006@gmail.com>
2023-08-20 19:42:18 +0200

    group add, scrollable list

--- SSH-Configurer-UI/Services/GroupService.cs
+++ SSH-Configurer-UI/Services/GroupService.cs
@@ -1,10 +1,12 @@
 ﻿using SSH_Configurer_UI.Model;
+using SSH_Configurer_UI.Pages;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SSH_Configurer_UI.Services
 {
     public class GroupService
     {
-        private static readonly Group[] Groups = new[]
+        private List<Group> Groups = new()
         {
             new Group(0, "Room1", 0, new List<int>(){0,1,2}),
             new Group(1, "Room1", 1, new List<int>(){3,4})
@@ -13,7 +15,20 @@ namespace SSH_Configurer_UI.Services
 
         public List<Group> GetAllGroups()
         {
-            return new List<Group>(Groups);
+            return Groups;
+        }
+
+        public List<Group> SearchByName(string input)
+        {
+            List<Group> filtered = Groups.Where(group => group.Name.ToLower().Contains(input.ToLower())).ToList();
+
+            return filtered;
+        }
+
+        public int AddGroup(Group group)
+        {
+            Groups.Add(group);
+            return 0;
         }
     }
 }
