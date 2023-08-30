3e406dcac9019d6841a74831092ff25d43ce3639
Paweł Durkalec <pdurkalec1006@gmail.com>
2023-08-30 21:27:40 +0200

    key pairs crud add

---
+++ SSH-Configurer-UI/Model/DeviceStatus.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSH_Configurer_UI.Model
+{
+    public class DeviceStatus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hostname { get; set; }
+        public string Status { get; set; }
+        public DeviceStatus() { }
+        public DeviceStatus(int id, string name, string hostname, string status)
+        {
+            Id = id;
+            Name = name;
+            Hostname = hostname;
+            Status = status;
+        }
+        public DeviceStatus(Device device)
+        {
+            Id = device.Id;
+            Name = device.Name;
+            Hostname = device.Hostname;
+            Status = "Initializing";
+        }
+    }
+}
